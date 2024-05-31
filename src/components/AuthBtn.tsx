0e39e1dc195331ce2f44f64b503467331e4b6407
Noah Vass <90555284+Noahsignt@users.noreply.github.com>
2024-05-31 17:17:19 +1000

    google-auth: basic header styling

--- src/components/AuthBtn.tsx
+++ src/components/AuthBtn.tsx
@@ -1,13 +1,14 @@
 
 interface AuthBtnProps {
     text: string,
-    onClick: () => void
+    onClick: () => void,
+    isTransparent: boolean
 }
 
 export const AuthBtn = (props: AuthBtnProps) => {
     return (
         <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className={`rounded-full ${props.isTransparent ? "bg-transparent" : "bg-white/20"} px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20`}
         onClick={props.onClick}
         >
             {props.text}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ export const Header = () => {
     const { data: sessionData } = useSession();
 
     return (
-        <header className="flex justify-between items-start bg-fuchsia-500">
-            <h1 className="text-2xl font-bold">
+        <header className="flex justify-between items-center bg-fuchsia-500">
+            <h1 className="text-2xl font-bold px-4">
                 Lyredium
             </h1>
             <div className="flex items-center">
-                <AuthBtn text={sessionData ? "Sign out" : "Get started"} onClick={sessionData ? () => void signOut() : () => void signIn()}/>
+                <AuthBtn text={sessionData ? "Sign out" : "Get started"} 
+                onClick={sessionData ? () => void signOut() : () => void signIn()}
+                isTransparent={true}/>
             </div>
         </header>
     )
