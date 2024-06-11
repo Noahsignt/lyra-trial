85f40b25a3c463db316b127aba980bd0647697f6
Noah Vass <90555284+Noahsignt@users.noreply.github.com>
2024-06-11 16:40:02 +1000

    profile-picture: fix extra useEffect

--- src/pages/user/[email].tsx
+++ src/pages/user/[email].tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from 'next/image';
-import { useState, useEffect, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { api } from "~/utils/api";
 
