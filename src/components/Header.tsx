37ca54d151ebc7349f816da301e83ddea4fe5dce
Noah Vass <90555284+Noahsignt@users.noreply.github.com>
2024-06-12 11:56:36 +1000

    search-bar: initial FE search bar + portal page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 import { api } from "~/utils/api";
 import { cacheBustImgURL } from "~/utils/format";
@@ -27,22 +28,44 @@ const ProfileSelectionSVG = () => {
 }
 
 const StoriesSelectionSVG = () => {
-    return(
+    return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" aria-label="Stories"><path d="M4.75 21.5h14.5c.14 0 .25-.11.25-.25V2.75a.25.25 0 0 0-.25-.25H4.75a.25.25 0 0 0-.25.25v18.5c0 .14.11.25.25.25z" stroke="currentColor"></path><path d="M8 8.5h8M8 15.5h5M8 12h8" stroke="currentColor" stroke-linecap="round"></path></svg>
+    )
+}
+
+const SearchBarSVG = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" className="text-gray-600" aria-label="Search"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.1 11.06a6.95 6.95 0 1 1 13.9 0 6.95 6.95 0 0 1-13.9 0zm6.94-8.05a8.05 8.05 0 1 0 5.13 14.26l3.75 3.75a.56.56 0 1 0 .8-.79l-3.74-3.73A8.05 8.05 0 0 0 11.04 3v.01z" fill="currentColor"></path></svg>
     )
 }
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [search, setSearch] = useState('');
+
     const { data: sessionData } = useSession();
     const email = sessionData?.user?.email ?? '';
-    const { data: userData, isLoading } = api.user.getUserByEmail.useQuery({ email }, { enabled: !!email });
+    const { data: userData } = api.user.getUserByEmail.useQuery({ email }, { enabled: !!email });
+
+    const router = useRouter();
+
+    const handleSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            if (search) {
+                router.push(`/search?q=${search}`);
+            }
+        }
+    }
 
     return (
         <header className="flex justify-between items-center border-b-2 border-gray-100 h-12 px-2">
-            <Link className="text-2xl font-bold px-4" href="/">
-                Lyredium
-            </Link>
+            <div className="flex items-center gap-2">
+                <Link className="text-2xl font-bold px-4" href="/">
+                    Lyredium
+                </Link>
+                <SearchBarSVG />
+                <input type="text" placeholder="Search" className="bg-transparent p-2 rounded-md w-48 text-sm outline-none text-gray-600" onChange={(e) => setSearch(e.target.value)} onKeyDown={(e) => handleSearchKeyDown(e)}/>
+            </div>
             {sessionData ?
             <div className="flex items-center gap-4">
                 <WriteButton />
